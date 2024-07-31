5fcc79235a9760e35de873b7b10ab0c8a9666acc
sahiltemkar <sahiltemkar321@gmail.com>
2024-08-01 00:24:54 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Charlie',
+    image: '/images/airpods.jpg',
+    description:
+      'Charlie is a lively Golden Retriever, full of energy and affection. At 2 years old, he loves playing fetch and enjoys long walks in the park. This friendly canine companion is always ready for an adventure.',
+    brand: 'Golden',
+    category: 'DOG',
+    price: 89.99,
+    countInStock: 5,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'Bentley',
+    image: '/images/phone.jpg',
+    description:
+      'Bentley, the energetic Siberian Husky, is 1 year old and has striking blue eyes that capture hearts. Known for his friendly nature and love for outdoor activities, Bentley is a true adventurer and a furry friend for life.',
+    brand: 'Siberian Husky',
+    category: 'DOG',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Daisy',
+    image: '/images/camera.jpg',
+    description:
+      'Daisy, the playful Bulldog, is 2 years old and has a heart as big as her wrinkles. Her endearing snorts and wiggly walk bring laughter to everyone around, making her a beloved member of the family.',
+    brand: 'Bulldog',
+    category: 'DOG',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Whiskers',
+    image: '/images/playstation.jpg',
+    description:
+      'Whiskers, our elegant Siamese cat, is 3 years old and enjoys lounging in sunny spots around the house. With striking blue eyes and a gentle demeanor, Whiskers is the purrfect addition to any home.',
+    brand: 'CAT',
+    category: 'CAT',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Rocky',
+    image: '/images/mouse.jpg',
+    description:
+      'Rocky, a 2-year-old German Shepherd, is known for his intelligence and protective instincts. Whether playing fetch in the backyard or keeping watch over the house, Rocky is a loyal and loving companion.',
+    brand: 'German Shepherd',
+    category: 'DOG',
+    price: 149.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Lola',
+    image: '/images/alexa.jpg',
+    description:
+      'Lola, the adorable Shih Tzu, is 4 years old and loves to be pampered. Her fluffy coat and sweet personality make her a delightful addition to any family, bringing warmth and happiness wherever she goes.',
+    brand: 'Shih Tzu',
+    category: 'DOG',
+    price: 629.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
